b881870fbef04890e9f6537ded4a2aa178b008d2
HoangDo <hoangdo1@beeketing.net>
2023-12-23 23:30:57 +0700

    update

--- src/Http/Controllers/WebhookWordpressController.php
+++ src/Http/Controllers/WebhookWordpressController.php
@@ -6,6 +6,7 @@ use Dvhoangfh\Aepay\Events\OrderCreated;
 use Dvhoangfh\Aepay\Models\WordpressOrder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Api;
 
 class WebhookWordpressController extends Controller
 {
@@ -20,6 +21,31 @@ class WebhookWordpressController extends Controller
             }
             $order->save();
             OrderCreated::dispatch($order->order_id);
+            try {
+                $telegram = new Api(config('services.telegram-bot-api.token'));
+                $data = [
+                    'service'  => 'WP',
+                    'order_id' => $order->order_id,
+                    'package'  => $order->package->name,
+                    'email'    => $order->customer->email,
+                    'amount'   => $order->amount,
+                ];
+                $telegram->sendMessage(
+                    [
+                        'chat_id'    => config('services.telegram-bot-api.chat_id'),
+                        'parse_mode' => 'HTML',
+                        'text'       => "<strong>App name: </strong>" . "Aesport TV" . "\n" .
+                            "<strong>Via : </strong>" . $data['service'] . "\n" .
+                            "<strong>Order ID : </strong>" . $data['order_id'] . "\n" .
+                            "<strong>Package : </strong>" . $data['package'] . "\n" .
+                            "<strong>Amount : </strong>" . $data['amount'] . "\n" .
+                            "<strong>Email : </strong>" . $data['email'] . "\n" .
+                            "<i>Message has sent form <b>Aesport Team</b></i>"
+                    ]
+                );
+            } catch (\Exception $exception) {
+                Log::error('send telegram message error' . $exception->getMessage());
+            }
         }
         return \response()->json($payload);
     }
